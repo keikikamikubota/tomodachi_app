a8032e7110342c2e3b64e5d307762588148f286e
keiki <kkeki0410@gmail.com>
2023-08-09 10:49:29 +0900

    newとeditでconfirm画面の切り替えを実装

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,10 +1,10 @@
 class FeedsController < ApplicationController
-  before_action :set_feed, only: %i[ show edit update destroy ]
   def index
     @feeds = Feed.all
   end
 
   def show
+    @feed = Feed.find(params[:id])
   end
 
   def new
@@ -20,47 +20,38 @@ class FeedsController < ApplicationController
   end
 
   def edit
+    @feed = Feed.find(params[:id])
   end
 
   def create
     @feed = Feed.new(feed_params)
-
-    respond_to do |format|
+    if params[:back]
+      render :new
+    else
       if @feed.save
-        format.html { redirect_to feed_url(@feed), notice: "Feed was successfully created." }
-        format.json { render :show, status: :created, location: @feed }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
+        redirect_to feeds_path, notice: "投稿が完了しました！"
+      else 
+        render :new
       end
     end
   end
 
   def update
-    respond_to do |format|
-      if @feed.update(feed_params)
-        format.html { redirect_to feed_url(@feed), notice: "Feed was successfully updated." }
-        format.json { render :show, status: :ok, location: @feed }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
+    @feed = Feed.find(params[:id])
+    if @feed.update(feed_params)
+      redirect_to feeds_path, notice: "編集が完了しました！"
+    else
+      render :edit
     end
   end
 
   def destroy
+    @feed = Feed.find(params[:id])
     @feed.destroy
-    respond_to do |format|
-      format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to feeds_path
   end
 
   private
-
-    def set_feed
-      @feed = Feed.find(params[:id])
-    end
 
     def feed_params
       params.require(:feed).permit(:image, :image_cache)
